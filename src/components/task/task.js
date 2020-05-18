fc28304df828fd710a4662a54d8ff8b323ff513f
Ellie Judge <ellie.judge@techreturners.com>
2020-05-18 18:08:10 +0100

    added date picker but currently not working

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -4,16 +4,16 @@ import './task.css';
 import Checkbox from '@material-ui/core/Checkbox';
 import CloseIcon from '@material-ui/icons/Close';
 import Clock from '../assets/clock.png';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-
-
-
-function Task(props){
-       const [closeIconSize, setCloseIconSize] = useState("small")
+function Task(props) {
+    const [closeIconSize, setCloseIconSize] = useState();
+    const [startDate, setStartDate] = useState(new Date());
 
     const handleChecked = (event) => {
         props.completeTaskFunc(props.id, event.target.checked)
-      };
+    };
 
     const handleDelete = () => {
         props.deleteTaskFunc(props.id)
@@ -28,46 +28,46 @@ function Task(props){
     }
 
     const handleClockClick = () => {
-        console.log("clock clicked")
-
+        setTime( <DatePicker selected={startDate} onChange={date => setStartDate(date)} />)
     }
-   
-
-const checkBoxStyles = theme => ({
-    root: {
-      '&$checked': {
-        color: "rgb(74,168,185)",
-      },
-    },
-    checked: {},
-   })
-
-const CustomCheckbox = withStyles(checkBoxStyles)(Checkbox);
+    
+    const checkBoxStyles = theme => ({
+        root: {
+            '&$checked': {
+                color: "rgb(74,168,185)",
+            },
+        },
+        checked: {},
+    })
+    
+    const [time, setTime] = useState(<img src={Clock} className="clock" alt="clockImage" onClick={handleClockClick} />)
+    const CustomCheckbox = withStyles(checkBoxStyles)(Checkbox);
 
     return (
         <>
-        <div style={{display:"flex", justifyContent:"center"}}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
 
-        {props.completed === false ? <h1 className="todo-line">{props.text}</h1> : 
-        <span style={{color: 'rgb(237,117,140)', textDecoration: 'line-through'}}><span style={{ color: 'black'}}><h1>{props.text}</h1></span></span>}
+                {props.completed === false ? <h1 className="todo-line">{props.text}</h1> :
+                    <span style={{ color: 'rgb(237,117,140)', textDecoration: 'line-through' }}><span style={{ color: 'black' }}><h1>{props.text}</h1></span></span>}
 
-        <img src={Clock} className="clock" alt="clockImage" onClick={handleClockClick}/>
+                {time}
 
-        <CustomCheckbox 
-        checked={props.completed}
-        onChange={handleChecked}
-        />
+                <CustomCheckbox
+                    checked={props.completed}
+                    onChange={handleChecked}
+                />
 
+                <CloseIcon
+                    onClick={handleDelete}
+                    onMouseEnter={handleMouseEnterCloseIcon}
+                    onMouseLeave={handleMouseLeaveCloseIcon}
+                    color="secondary"
+                    fontSize={closeIconSize}
+                    style={{ height: "40px", marginTop: "24px" }}
+                />
 
-        <CloseIcon
-        onClick={handleDelete}
-        onMouseEnter={handleMouseEnterCloseIcon}
-        onMouseLeave={handleMouseLeaveCloseIcon}
-        color="secondary"
-        fontSize={closeIconSize}
-        style={{ height: "40px", marginTop:"24px"}}
-        />
-        </div>
+                
+            </div>
         </>
     )
 };
