1d4e16872dfcfcbd51556ce0766a8ecef627b818
Ellie Judge <ellie.judge@techreturners.com>
2020-09-22 20:36:23 +0100

    First commit maybe.. It's been a while

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -9,7 +9,6 @@ import "react-datepicker/dist/react-datepicker.css";
 
 function Task(props) {
     const [closeIconSize, setCloseIconSize] = useState();
-    const [startDate, setStartDate] = useState(new Date());
 
     const handleChecked = (event) => {
         props.completeTaskFunc(props.id, event.target.checked)
@@ -26,9 +25,10 @@ function Task(props) {
     const handleMouseLeaveCloseIcon = () => {
         setCloseIconSize("small")
     }
+    const [startDate, setStartDate] = useState(new Date());
 
     const handleClockClick = () => {
-        setTime( <DatePicker selected={startDate} onChange={date => setStartDate(date)} />)
+        setTime( <DatePicker selected={startDate} onChange={date => setStartDate(date)} />) // conditional rendering (in return show date picker)
     }
     
     const checkBoxStyles = theme => ({
@@ -42,7 +42,7 @@ function Task(props) {
     
     const [time, setTime] = useState(<img src={Clock} className="clock" alt="clockImage" onClick={handleClockClick} />)
     const CustomCheckbox = withStyles(checkBoxStyles)(Checkbox);
-
+    
     return (
         <>
             <div style={{ display: "flex", justifyContent: "center" }}>
@@ -50,12 +50,14 @@ function Task(props) {
                 {props.completed === false ? <h1 className="todo-line">{props.text}</h1> :
                     <span style={{ color: 'rgb(237,117,140)', textDecoration: 'line-through' }}><span style={{ color: 'black' }}><h1>{props.text}</h1></span></span>}
 
+                <div>
                 {time}
+                        
 
                 <CustomCheckbox
                     checked={props.completed}
                     onChange={handleChecked}
-                />
+                    />
 
                 <CloseIcon
                     onClick={handleDelete}
@@ -64,9 +66,10 @@ function Task(props) {
                     color="secondary"
                     fontSize={closeIconSize}
                     style={{ height: "40px", marginTop: "24px" }}
-                />
+                    />
 
-                
+                {/* <DatePicker selected={startDate} onChange={date => setStartDate(date)} /> */}
+                    </div>
             </div>
         </>
     )
